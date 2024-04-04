460919adc799d13f832153a9a4b1f95bfd50c0a0
BrivaHamisi <brivahamisi@gmail.com>
2024-04-04 18:49:55 +0300

    React with and without JSX

--- src/components/Hallo.js
+++ src/components/Hallo.js
@@ -1,11 +1,13 @@
- import React from "react";
+import React from "react";
 
- const Hello = () => {
-    return (
-        <div>
-            <h1> Hello Hamisi</h1>
-        </div>
-    )
- }
+const Hello = () => {
+   // return (
+   //     <div className='dummyClass'>
+   //         <h1> Hello Hamisi</h1>
+   //     </div>
+   // )
 
- export default Hello
+   return React.createElement('div', {id: 'hello', className:'dummyClass'}, React.createElement('h1', null, 'Hello Hamisi'))
+}
+
+export default Hello
